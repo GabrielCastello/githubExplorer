1c45ccc21f5cab86feff267028c4c2a2e1bd5d27
Gabriel Castello <gabriel__castello@hotmail.com>
2021-08-19 21:01:37 -0300

    Created RepositoryItem componente and updated RepositoryList to use it

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,22 @@
-const RepositoryName = 'unform1'
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository = {
+  name: 'nnform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList () {
   return (
     <section className="repositoryList">
       <h1>Repos List</h1>
       <ul>
-        <li>
-          <strong>{RepositoryName}</strong>
-          <p> Forms in React</p>
-          <a href="">Access Repo</a>
-        </li>
+        <RepositoryItem
+          repository={repository}/>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   )
